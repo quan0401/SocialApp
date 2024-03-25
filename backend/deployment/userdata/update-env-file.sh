5e2b8943c5e42eda3657fa5316d41b441e4c5c48
Đồng Minh Quân <dongminhquan2004@gmail.com>
2024-03-25 08:17:54 +0700

    install zip for CircleCI

--- backend/deployment/userdata/update-env-file.sh
+++ backend/deployment/userdata/update-env-file.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+function program_is_installed {
+  local return_=1
+
+  type $1 >/dev/null 2>&1 || { local return_=0; }
+  echo "$return_"
+}
+
+if [ $(program_is_installed zip) == 0 ]; then
+  apk update
+  apk add zip
+fi
+
 aws s3 sync s3://socialapp-env-files/develop .
 unzip env-file.zip
 cp .env.develop .env
